b68aa39dbb20421c33c802466638c863a51fd005
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-02 18:21:44 +0100

    fixing type error

--- transpiled/automaticInferSwitch.js
+++ transpiled/automaticInferSwitch.js
@@ -1,4 +1,24 @@
 "use strict";
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spread = (this && this.__spread) || function () {
+    for (var ar = [], i = 0; i < arguments.length; i++) ar = ar.concat(__read(arguments[i]));
+    return ar;
+};
 var Add = /** @class */ (function () {
     function Add(payload) {
         this.payload = payload;
@@ -24,7 +44,7 @@ function todoReducer(action, state) {
     switch (action.type) {
         case "Add": {
             return {
-                todos: state.todos.concat([action.payload])
+                todos: __spread(state.todos, [action.payload])
             };
         }
         case "Remove All": {
